8a88b762d08971601086cf58fa957ca77fe3d056
estebanana2002 <utti212031@utvco.edu.mx>
2024-08-18 02:01:51 -0600

    Add: define TypeScript interfaces for cart and cart item data structure

---
+++ src/data/interfaces/cart.interfaces.ts
@@ -0,0 +1,9 @@
+import { Status } from "../enums/status.enum";
+
+export interface Cart {
+    id: string;
+    title: string;
+    description: string;
+    created_at: string;
+    status: Status;
+}
